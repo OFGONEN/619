d238826516755e5615471e145d27cde711d913f4
Ömer Faruk Gönen <ofgonen@gmail.com>
2018-07-09 08:33:36 +0300

    Game Documents and Files are Added

---
+++ Assets/OnPressed.cs
@@ -0,0 +1,42 @@
+﻿/*
+Created By OFGONEN
+*/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class OnPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+	#region Variables
+	public SpriteChanger start;
+	public SpriteChanger how2play;
+	public SpriteChanger options;
+	public SpriteChanger wtfAreWe;
+
+	#endregion
+	
+	
+
+	#region Methods
+	public void OnPointerDown( PointerEventData eventData )
+	{
+		Debug.Log( gameObject.name + " Pointer Down" );
+
+		start.PointerDown();
+		how2play.PointerDown();
+		options.PointerDown();
+		wtfAreWe.PointerDown();
+	}
+
+	public void OnPointerUp( PointerEventData eventData )
+	{
+		Debug.Log( gameObject.name + " Pointer Down" );
+
+		start.PointerUp();
+		how2play.PointerUp();
+		options.PointerUp();
+		wtfAreWe.PointerUp();
+	}
+	#endregion
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-619
+619 ' s idea is created by M6hd9
+and developed by both OFGONEN && M6hd9
+
+619 is a game where you can score when you make 619 with numbers.
+If you make enough score in one round you get to go Up Side Down.
+In upside down only way to do score is 616 or 919.
+When table gets full player with more score wins the game.
