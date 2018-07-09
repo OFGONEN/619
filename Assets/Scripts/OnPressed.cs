00493b397c30741562a589e4f235cb487d0d8d38
Ömer Faruk Gönen <ofgonen@gmail.com>
2018-07-09 13:07:41 +0300

    Build Folder & Build v1.0

---
+++ Assets/Scripts/OnPressed.cs
@@ -0,0 +1,58 @@
+﻿/*
+Created By OFGONEN
+*/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class OnPressed : MonoBehaviour, IPointerDownHandler, IPointerUpHandler , IPointerExitHandler
+{
+
+	#region Variables
+	public SpriteChanger start;
+	public SpriteChanger how2play;
+	public SpriteChanger options;
+	public SpriteChanger wtfAreWe;
+
+	#endregion
+	
+	
+
+	#region Methods
+	public void OnPointerDown( PointerEventData eventData )
+	{
+		Debug.Log( gameObject.name + " Pointer Down" );
+		GoNegative();
+	}
+
+	public void OnPointerUp( PointerEventData eventData )
+	{
+		Debug.Log( gameObject.name + " Pointer Up" );
+		GoNormal();
+	}
+
+	public void OnPointerExit( PointerEventData eventData )
+	{
+		Debug.Log( gameObject.name + " Pointer Exit" );
+		GoNormal();		
+	}
+
+	void GoNegative()
+	{
+		start.PointerDown();
+		how2play.PointerDown();
+		options.PointerDown();
+		wtfAreWe.PointerDown();
+		Camera.main.backgroundColor = Color.black;
+	}
+
+	void GoNormal()
+	{
+		start.PointerUp();
+		how2play.PointerUp();
+		options.PointerUp();
+		wtfAreWe.PointerUp();
+		Camera.main.backgroundColor = Color.white;
+	}
+	#endregion
+}
